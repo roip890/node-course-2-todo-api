ff477fe498451b111405486028b145ad8db878c0
roip890 <roip890@gmail.com>
2018-06-27 14:57:34 +0300

    heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started on port ' + port);
 });
 
 module.exports = {app};
