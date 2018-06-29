1440eb977116ce882f75df8e8c78fba0c2296846
roip890 <roip890@gmail.com>
2018-06-29 13:06:57 +0300

    config

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,18 @@
-var env = process.env.NODE_ENV;
+var env = process.env.NODE_ENV || "development";
 
-if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env !== 'production') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+  console.log(Object.keys(envConfig));
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// } else if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }
